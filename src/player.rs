a201073fd3bb467dceeebc6bd35c2f1f63e7e1bf
Bartek Jaszczak <bartlomiej.jaszczak@proton.me>
2024-03-09 12:14:46 +0100

    fix: Modify minimax so it considers depth
    Minimax wouldn't take depth into account and therefore it wouldn't
    prevent immediate lose if it was lost anyway (it didn't prolong
    the game as far as possible if losing). For the same reason it
    sometimes wouldn't choose immediate win over next turn win.
    
    Also added tests for minimax algorithm.

--- src/player.rs
+++ src/player.rs
@@ -44,7 +44,7 @@ impl Player {
             let mut next_board = board.clone();
             next_board[board_move.index()] = maximizing_player_symbol;
 
-            let score = Player::minimax(&next_board, &maximizing_player_symbol, false);
+            let score = Player::minimax(&next_board, &maximizing_player_symbol, false, 1);
             if score > best_eval {
                 best_eval = score;
                 best_moves.clear();
@@ -59,14 +59,19 @@ impl Player {
         best_moves[random_index]
     }
 
-    fn minimax(board: &Board, maximizing_player_symbol: &Cell, is_maximizing: bool) -> i32 {
+    fn minimax(
+        board: &Board,
+        maximizing_player_symbol: &Cell,
+        is_maximizing: bool,
+        depth: i32,
+    ) -> i32 {
         if let Some(winning_line_index) = board.get_winning_line() {
             let winner_symbol = board[WINNING_LINES[winning_line_index][0]];
 
             return if &winner_symbol == maximizing_player_symbol {
-                10
+                100 - depth
             } else {
-                -10
+                depth - 100
             };
         } else if board.is_full() {
             return 0;
@@ -83,7 +88,12 @@ impl Player {
         for board_move in board.get_possible_moves() {
             let mut next_board = board.clone();
             next_board[board_move.index()] = current_player_symbol;
-            let value = Player::minimax(&next_board, maximizing_player_symbol, !is_maximizing);
+            let value = Player::minimax(
+                &next_board,
+                maximizing_player_symbol,
+                !is_maximizing,
+                depth + 1,
+            );
             best_score = cmp_function(best_score, value);
         }
         return best_score;
@@ -96,12 +106,12 @@ mod tests {
     use crate::game::GameResult;
 
     struct MockUi {
-        returned_move: BoardMove,
+        returned_move: Option<BoardMove>,
     }
 
     impl Ui for MockUi {
         fn get_move(&self, _player_name: &str, _additional_message: Option<&str>) -> BoardMove {
-            self.returned_move.clone()
+            self.returned_move.unwrap().clone()
         }
 
         fn update_board(&self, _board: &Board) {
@@ -117,7 +127,7 @@ mod tests {
     fn get_human_move() {
         let returned_move = BoardMove::try_new(3).unwrap();
         let mock_ui = MockUi {
-            returned_move: returned_move.clone(),
+            returned_move: Some(returned_move.clone()),
         };
         let fake_board = Board::new();
         let player = Player::Human(String::from("Steve"));
@@ -128,15 +138,244 @@ mod tests {
     }
 
     #[test]
-    #[ignore = "Ignored until it's rewritten"]
-    fn get_cpu_move() {
-        // TODO: Update the test case once the functionality is in place
-        let returned_move = BoardMove::try_new(3).unwrap();
-        let mock_ui = MockUi { returned_move };
-        let fake_board = Board::new();
+    fn cpu_makes_valid_moves() {
+        let mock_ui = MockUi {
+            returned_move: None,
+        };
+        let boards = vec![
+            Board::new(),
+            Board::from([
+                Cell::O,
+                Cell::O,
+                Cell::X,
+                Cell::X,
+                Cell::Empty('5'),
+                Cell::Empty('6'),
+                Cell::Empty('7'),
+                Cell::Empty('8'),
+                Cell::Empty('9'),
+            ]),
+            Board::from([
+                Cell::O,
+                Cell::Empty('2'),
+                Cell::Empty('3'),
+                Cell::Empty('4'),
+                Cell::O,
+                Cell::Empty('6'),
+                Cell::Empty('7'),
+                Cell::X,
+                Cell::Empty('9'),
+            ]),
+        ];
 
         let cpu = Player::CPU;
 
-        let _m = cpu.get_move(&fake_board, &mock_ui, None);
+        for board in boards {
+            let m = cpu.get_move(&board, &mock_ui, None);
+
+            assert!(
+                board.is_valid_move(&m),
+                "CPU should always return a valid move",
+            );
+        }
+    }
+
+    #[test]
+    fn cpu_wins_whenever_possible() {
+        let mock_ui = MockUi {
+            returned_move: None,
+        };
+        let boards_and_expected_moves = vec![
+            (
+                Board::from([
+                    Cell::O,
+                    Cell::Empty('2'),
+                    Cell::X,
+                    Cell::O,
+                    Cell::O,
+                    Cell::Empty('6'),
+                    Cell::Empty('7'),
+                    Cell::Empty('8'),
+                    Cell::X,
+                ]),
+                BoardMove::try_new(6).unwrap(),
+            ),
+            // (
+            //     Board::from([
+            //         Cell::Empty('1'),
+            //         Cell::X,
+            //         Cell::X,
+            //         Cell::Empty('4'),
+            //         Cell::O,
+            //         Cell::X,
+            //         Cell::O,
+            //         Cell::Empty('8'),
+            //         Cell::O,
+            //     ]),
+            //     BoardMove::try_new(8).unwrap(),
+            // ),
+            // (
+            //     Board::from([
+            //         Cell::O,
+            //         Cell::X,
+            //         Cell::Empty('3'),
+            //         Cell::O,
+            //         Cell::X,
+            //         Cell::Empty('6'),
+            //         Cell::Empty('7'),
+            //         Cell::Empty('8'),
+            //         Cell::Empty('9'),
+            //     ]),
+            //     BoardMove::try_new(7).unwrap(),
+            // ),
+        ];
+
+        let cpu = Player::CPU;
+
+        for (board, expected_move) in boards_and_expected_moves {
+            let m = cpu.get_move(&board, &mock_ui, None);
+
+            assert_eq!(
+                m, expected_move,
+                "CPU should always make winning move whenever possible"
+            );
+        }
+    }
+
+    #[test]
+    fn cpu_prevents_player_win() {
+        let mock_ui = MockUi {
+            returned_move: None,
+        };
+        let boards_and_expected_moves = vec![
+            // In this first case CPU is lost nonetheless, but putting an 'X' in cell 7 prolongs
+            // the game
+            (
+                Board::from([
+                    Cell::O,
+                    Cell::Empty('2'),
+                    Cell::X,
+                    Cell::O,
+                    Cell::Empty('5'),
+                    Cell::Empty('6'),
+                    Cell::Empty('7'),
+                    Cell::Empty('8'),
+                    Cell::Empty('9'),
+                ]),
+                BoardMove::try_new(7).unwrap(),
+            ),
+            (
+                Board::from([
+                    Cell::X,
+                    Cell::O,
+                    Cell::O,
+                    Cell::Empty('4'),
+                    Cell::X,
+                    Cell::Empty('6'),
+                    Cell::Empty('7'),
+                    Cell::Empty('8'),
+                    Cell::O,
+                ]),
+                BoardMove::try_new(6).unwrap(),
+            ),
+            (
+                Board::from([
+                    Cell::O,
+                    Cell::X,
+                    Cell::O,
+                    Cell::X,
+                    Cell::X,
+                    Cell::Empty('6'),
+                    Cell::Empty('7'),
+                    Cell::O,
+                    Cell::Empty('9'),
+                ]),
+                BoardMove::try_new(6).unwrap(),
+            ),
+        ];
+
+        let cpu = Player::CPU;
+
+        for (board, expected_move) in boards_and_expected_moves {
+            let m = cpu.get_move(&board, &mock_ui, None);
+
+            assert_eq!(
+                m, expected_move,
+                "CPU should always prevent other player from winning whenever possible"
+            );
+        }
+    }
+
+    #[test]
+    fn cpu_moves_in_last_free_cell() {
+        let mock_ui = MockUi {
+            returned_move: None,
+        };
+        let boards_and_expected_moves = vec![
+            (
+                Board::from([
+                    Cell::O,
+                    Cell::Empty('2'),
+                    Cell::X,
+                    Cell::X,
+                    Cell::O,
+                    Cell::O,
+                    Cell::O,
+                    Cell::X,
+                    Cell::X,
+                ]),
+                BoardMove::try_new(2).unwrap(),
+            ),
+            (
+                Board::from([
+                    Cell::O,
+                    Cell::O,
+                    Cell::X,
+                    Cell::X,
+                    Cell::X,
+                    Cell::O,
+                    Cell::O,
+                    Cell::X,
+                    Cell::Empty('9'),
+                ]),
+                BoardMove::try_new(9).unwrap(),
+            ),
+        ];
+
+        let cpu = Player::CPU;
+
+        for (board, expected_move) in boards_and_expected_moves {
+            let m = cpu.get_move(&board, &mock_ui, None);
+
+            assert_eq!(
+                m, expected_move,
+                "CPU should always move in last cell if it's the only one available"
+            );
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn panics_on_full_board() {
+        let mock_ui = MockUi {
+            returned_move: None,
+        };
+        let full_board = Board::from([
+            Cell::O,
+            Cell::O,
+            Cell::X,
+            Cell::X,
+            Cell::X,
+            Cell::O,
+            Cell::O,
+            Cell::X,
+            Cell::O,
+        ]);
+
+        assert!(full_board.is_full(), "Board must be full for this test");
+
+        let cpu = Player::CPU;
+
+        cpu.get_move(&full_board, &mock_ui, None);
     }
 }
